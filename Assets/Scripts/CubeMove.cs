b823ac7af7067d5ad71343dd8910cd3b99251c96
yaufdd <ericozavr@gmail.com>
2022-05-17 00:49:42 +0300

    finally cubes move with music

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -1,38 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CubeMove : MonoBehaviour
 {
     public Conductor conductor;
     public AudioClip music;
 
+    public Vector3 spawnPos;
     public Vector3 removePos;
 
-    public GameManager gameManager;
+    //public GameManager gameManager;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPos = transform.position;
     }
 
     private void Update() {
-        for (int i = 0;i < gameManager.notes.Count; i++){
-            Vector3 spawnPos = new Vector3(PlayerPrefs.GetFloat($"{music.name}_{i}_position_x"),
-                                            PlayerPrefs.GetFloat($"{music.name}_{i}_position_y"),
-                                            PlayerPrefs.GetFloat($"{music.name}_{i}_position_z"));
-            gameManager.notes[i].transform.position = Vector3.Lerp(spawnPos, 
-            removePos, 
-            (conductor.BeatsShownInAdvance - (PlayerPrefs.GetFloat($"{music.name}_{i}") - conductor.songPosInBeats)) / conductor.BeatsShownInAdvance);
+        if (SceneManager.GetActiveScene().name == "PlayScene")
+        {
+            MoveWithMusic();
         }
     }
 
-    // private void MoveWithMusic(Vector3 spawnPos, Vector3 removePos, int numberOfNote){
-    //     transform.position = Vector3.Lerp(
-    //     spawnPos,
-    //     removePos,
-    //     (conductor.BeatsShownInAdvance - (PlayerPrefs.GetFloat($"{music.name}_{numberOfNote}") - conductor.songPosInBeats)) / conductor.BeatsShownInAdvance
-    // );    
-    // }
+    private void MoveWithMusic()
+    {
+        transform.position =  Vector3.Lerp(
+            spawnPos,
+            removePos,
+            (conductor.BeatsShownInAdvance - (PlayerPrefs.GetFloat($"{music.name}_{transform.name}_beatsOfNote") - conductor.songPosInBeats)) / conductor.BeatsShownInAdvance);
+    }
 }
